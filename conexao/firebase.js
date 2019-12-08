ab3cb6aaafeb5e6fcf102359024a02bcb314aef1
Gustavo <53282378+Gustavo-Diego@users.noreply.github.com>
2019-12-08 00:01:02 -0300

    trocando
    trocando

--- conexao/firebase.js
+++ conexao/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDTH-DPJ3lVVzCpuXRJZVpTZT_4ySAfspA",
-  authDomain: "teste-9f09d.firebaseapp.com",
-  databaseURL: "https://teste-9f09d.firebaseio.com",
-  projectId: "teste-9f09d",
-  storageBucket: "teste-9f09d.appspot.com",
-  messagingSenderId: "438572129305",
-  appId: "1:438572129305:web:0d6f7728fe1f6f4f391e88",
-  measurementId: "G-03VW6S07SY"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 const firebase = require('firebase')
